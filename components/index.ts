2381a931f548fa3680d70ba5a49687c8e7b1faed
Gad Nadjar <gadnadjar@gmail.com>
2024-03-13 12:07:35 +0200

    add function calculate price

--- components/index.ts
+++ components/index.ts
@@ -5,5 +5,15 @@ import NavBar from './NavBar';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacter from './SearchManufacter';
+import CarCard from './CarCard';
 
-export { Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter, SearchManufacter };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacter,
+  CarCard,
+};
