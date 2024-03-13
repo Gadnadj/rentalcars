b0718e96d81814e422d1753b7678310822d3ccf6
Gad Nadjar <gadnadjar@gmail.com>
2024-03-13 12:22:47 +0200

    fix bugs card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -24,8 +24,11 @@ const carCard = ({ car }: CarCardProps) => {
         <span className='self-start text-[14px] font-semibold'>$</span>
         {carRent}
         <span className='self-end text-[14px] font-medium'>/day</span>
-
       </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+      </div>
     </div>
   );
 };
