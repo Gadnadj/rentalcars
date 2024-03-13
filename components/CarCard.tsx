9bec78fdf8da70922a95836e21fcc3a021a2c4cd
Gad Nadjar <gadnadjar@gmail.com>
2024-03-13 16:35:09 +0200

    add cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -23,10 +23,14 @@ const carCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <p className='flex mt-6 text-[32px] font-extrabold'>
-        <span className='self-start text-[14px] font-semibold'>$</span>
+      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
+        <span className='self-start text-[14px] leading-[17px] font-semibold'>
+          $
+        </span>
         {carRent}
-        <span className='self-end text-[14px] font-medium'>/day</span>
+        <span className='self-end text-[14px] leading-[17px] font-medium'>
+          /day
+        </span>
       </p>
 
       <div className='relative w-full h-40 my-3 object-contain'>
@@ -39,7 +43,7 @@ const carCard = ({ car }: CarCardProps) => {
         />
       </div>
       <div className='relative flex w-full mt-2'>
-        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+        <div className='flex group-hover:invisible w-full justify-between text-grey'>
           <div className='flex flex-col justify-center items-center gap-2'>
             <Image
               src='/steering-wheel.svg'
