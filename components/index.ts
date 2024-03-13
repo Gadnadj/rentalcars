afc270420a0fd4048a65077fca5db1db2e320314
Gad Nadjar <gadnadjar@gmail.com>
2024-03-13 10:11:14 +0200

    add filter container

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
 
-export { Hero, CustomButton, NavBar, Footer };
+export { Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter };
